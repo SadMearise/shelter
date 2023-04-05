cd04468a46ad7b8642a5563e580d85324206bfcd
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2023-04-05 21:10:00 +0300

    feat: add burger menu functionality

---
+++ shelter/js/script.js
@@ -0,0 +1,30 @@
+const burgerMenu = function() {
+	const menu = document.querySelector('.icon-menu');
+	const headerBackground = document.querySelector('.header-nav-menu__background');
+	const headerBody = document.querySelector('.header-nav-menu__body');
+
+	menu.addEventListener('click', function() {
+		document.body.classList.toggle('lock');
+		menu.classList.toggle('active');
+		headerBackground.classList.toggle('active');
+		headerBody.classList.toggle('active');
+	})
+	
+	const removeClassActive = function() {
+		document.body.classList.remove('lock');
+		menu.classList.remove('active');
+		headerBackground.classList.remove('active');
+		headerBody.classList.remove('active');
+	}
+
+	headerBackground.addEventListener('click', removeClassActive);
+
+	const list = document.querySelector('.header-nav-menu__list');
+
+	list.addEventListener('click', (event) => {
+		const target = event.target;
+		if (target.tagName == 'A') removeClassActive();
+	})
+}
+
+burgerMenu();
